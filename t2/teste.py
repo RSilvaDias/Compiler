cd6db6112626e15f4f02fa9fd0cc5214a30dbc0e
Renato S Dias <39096504+RSilvaDias@users.noreply.github.com>
2022-03-16 23:37:58 -0300

    Add files via upload

--- t2/teste.py
+++ t2/teste.py
@@ -1,16 +1,25 @@
 import states
-import pandas
+import pandas as pd
 import functions
 import lexico
+import numpy as np
+
 
 
 file = open("test.txt", "r")
 
 for line in file:
-    print("teste : ",line)
     lexico.lexico(line)
 
-while lexico.tokens:
-    token = lexico.tokens.pop(0)
-    a = token.classe
-    print(a)
+
+ACTION = pd.read_csv('tabela_action.csv')
+ACTION.reset_index(drop = False, inplace = True)
+
+print(ACTION.to_string())
+print(ACTION.at[0,"inicio"])
+print(ACTION.at[int("2"),"varinicio"])
+
+#while lexico.tokens:
+#    token = lexico.tokens.pop(0)
+#    a = token.classe
+#    print(a)
